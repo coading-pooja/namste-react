87d913f9ed5d9bfc091d33808810916cd5f36dd0
Pooja Goyal <poojagoyal5106@gmail.com>
2024-04-30 17:19:41 +0530

    episode 1

---
+++ app.js
@@ -0,0 +1,7 @@
+const heading = React.createElement("h1",{id:"abc"},"hello world from React");
+    const root = ReactDOM.createRoot(document.getElementById("root"))
+
+    root.render(heading);   
+console.log(heading) 
+// console.log return object
+    // hello world from react using react
