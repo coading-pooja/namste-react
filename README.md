dc9410e3da62a2f4c39f38d02927f371e315bc52
Pooja Goyal <poojagoyal5106@gmail.com>
2024-05-02 11:11:39 +0530

    episode 2

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 namste-react
+#Parcel can do=>(
+dev build
+local server
+HMR -hot module replacement
+file watching algorithm - written in c++
+caching -faster builds
+image optimization
+minification
+bundling
+compress
+consistent hashing
+code splitting
+differential bundling
+suport older browsers
+beautiful error gives
+https
+tree shaking-remove unused code for u
+difference for dev and production)
--- app.js
+++ app.js
@@ -1,7 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
 const heading = React.createElement("h1",{id:"abc"},"hello world from React");
-    const root = ReactDOM.createRoot(document.getElementById("root"))
+    const root = ReactDOM.createRoot(document.getElementById("root"));
 
     root.render(heading);   
-console.log(heading) 
+console.log(heading)
 // console.log return object
     // hello world from react using react
